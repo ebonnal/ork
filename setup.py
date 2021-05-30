4cf5108072e6d0b387e9188fcad4730c5ddb5013
Enzo Bonnal <enzobonnal@gmail.com>
2021-05-30 18:11:07 +0200

    update licence version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['ork'],
     package_data={"ork": ["*.json"]},
     url='http://github.com/bonnal-enzo/ork',
-    license='Apache 2.',
+    license='Apache 2.0',
     author='Enzo Bonnal',
     author_email='enzobonnal@gmail.com',
     description='Warhammer 40k tool box'
